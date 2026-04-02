7286efb6350964d5d4e2281b259ea5a8252e7725
Polimanac <spolimana@tvz.hr>
2026-04-02 17:08:56 +0200

    Completed inventory domain model

---
+++ InventoryManagement/InventoryManagement.Console/Models/InventoryItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace InventoryManagement.Console.Models
+{
+    public class InventoryItem
+    {
+        public int Id { get; set; }
+        public int QuantityInStock { get; set; }
+        public int MinimumQuantity { get; set; }
+        public int MaximumQuantity { get; set; }
+        public string ShelfLocation { get; set; }
+        public DateTime LastCheckedAt { get; set; }
+
+        public Product Product { get; set; }
+        public Warehouse Warehouse { get; set; }
+    }
+}
